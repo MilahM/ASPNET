73cace2ae59dd6f865bd902e8891020874d36cf0
Kamilah <k.muhammad95@gmail.com>
2023-02-03 23:55:32 -0600

    ASPNET view product complete

--- Testing/Controllers/ProductController.cs
+++ Testing/Controllers/ProductController.cs
@@ -13,12 +13,16 @@ namespace Testing.Controllers
             this.repo = repo;
         }
 
-
         public IActionResult Index()
         {
             var products = repo.GetAllProducts();
             return View(products);
         }
-       
+        public IActionResult ViewProduct(int id)
+        {
+            var product = repo.GetProduct(id);
+            return View(product);
+        }
+
     }
 }
